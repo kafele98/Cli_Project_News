ac69e0e78953cb3e3806989f05f678ef888d29ca
kafele98 <60898398+kafele98@users.noreply.github.com>
2020-06-19 18:28:52 -0400

    Create protest_news_spec.rb

---
+++ spec/protest_news_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ProtestNews do
+  it "has a version number" do
+    expect(ProtestNews::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
